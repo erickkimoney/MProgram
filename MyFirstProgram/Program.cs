b21930cfcbdaf482e685f88c28cbbe73fd742253
Erick <Erick@DESKTOP-SHVMBM5>
2025-02-09 23:23:41 +0300

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Find the hypotenuse
+        Console.WriteLine("Enter side A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter side B: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double c = Math.Sqrt((a * a) + (b * b));
+        Console.WriteLine("The hypotenuse of this triagle is: " + c);
+    }
+}
