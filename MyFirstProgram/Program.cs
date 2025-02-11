1c0dd266597f9b9365519e5beb54f7b250ded2f0
Erick Macharia <machariaerickkimani@outlook.com>
2025-02-11 23:18:07 +0300

    Switch_statements

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,13 +6,90 @@ internal class Program
     private static void Main(string[] args)
     {
         //Find the hypotenuse
-        Console.WriteLine("Enter Triangle side A: ");
-        double a = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("Enter Triangle side A: ");
+        //double a = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Enter Triangle side B: ");
-        double b = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("Enter Triangle side B: ");
+        //double b = Convert.ToDouble(Console.ReadLine());
 
-        double c = Math.Sqrt((a * a) + (b * b));
-        Console.WriteLine("The hypotenuse of this triangle is: " + c);
+        //double c = Math.Sqrt((a * a) + (b * b));
+        //Console.WriteLine("The hypotenuse of this triangle is: " + c);
+
+        //String methods
+        String fullName = "Bro Code";
+        String phoneNumber = "123-456-7890";
+
+        //Console.WriteLine(fullName);
+        //fullName = fullName.ToUpper();
+        //Console.WriteLine(fullName);
+        //fullName = fullName.ToLower();
+        //Console.WriteLine(fullName);
+        //phoneNumber = phoneNumber.Replace("-", "/");
+        //Console.WriteLine(phoneNumber);
+        //String userName = fullName.Insert(0, "Mr. ");
+        //Console.WriteLine(userName);
+
+        //String firstName = fullName.Substring(0, 4);
+        //Console.WriteLine(firstName);
+
+        //if statements
+        //Console.WriteLine("What is your age: ");
+        //int age = Convert.ToInt32(Console.ReadLine());
+        //if (age > 100)
+        //{
+        //    Console.WriteLine("You are old");
+        //}
+        //else if (age > 18)
+        //{
+        //    Console.WriteLine("You are an adult");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("You are a minor");
+        //}
+        //Console.WriteLine("What is your name");
+        //String name = Console.ReadLine();
+        //if (name == "")
+        //{
+        //    Console.WriteLine("You havent entered your name!");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Your name is " + name);
+        //}
+        
+        //switch statements
+        Console.WriteLine("What day is it today?");
+        String day = Console.ReadLine();
+
+        switch (day)
+        {
+            case "Monday":
+                Console.WriteLine("Its Monday");
+                break;
+            case "Tuesday":
+                Console.WriteLine("Its Tuesday");
+                break;
+            case "Wednesday":
+                Console.WriteLine("Its Wednesday");
+                break;
+            case "Thursday":
+                Console.WriteLine("Its Thursday");
+                break;
+            case "Friday":
+                Console.WriteLine("Its Friday");
+                break;
+            case "Saturday":
+                Console.WriteLine("Its Saturday");
+                break;
+            case "Sunday":
+                Console.WriteLine("Its Sunday");
+                break;
+            default:
+                Console.WriteLine(day + " is not a day");
+                break;
+        }
+
+        Console.ReadKey();
     }
 }
---
+++ String_methods.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class StringMethods
+{
+    static void Main(string[] args)
+    {
+        String fullname = "Erik";
+        Console.WriteLine(fullname);
+    }
+}
