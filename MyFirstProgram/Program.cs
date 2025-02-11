1f758f24ecd4583f6f51375bdbd016504f7a70dd
Erick Macharia <machariaerickkimani@outlook.com>
2025-02-11 23:25:44 +0300

    Commenting switch statements

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -59,35 +59,35 @@ internal class Program
         //}
         
         //switch statements
-        Console.WriteLine("What day is it today?");
-        String day = Console.ReadLine();
+        //Console.WriteLine("What day is it today?");
+        //String day = Console.ReadLine();
 
-        switch (day)
-        {
-            case "Monday":
-                Console.WriteLine("Its Monday");
-                break;
-            case "Tuesday":
-                Console.WriteLine("Its Tuesday");
-                break;
-            case "Wednesday":
-                Console.WriteLine("Its Wednesday");
-                break;
-            case "Thursday":
-                Console.WriteLine("Its Thursday");
-                break;
-            case "Friday":
-                Console.WriteLine("Its Friday");
-                break;
-            case "Saturday":
-                Console.WriteLine("Its Saturday");
-                break;
-            case "Sunday":
-                Console.WriteLine("Its Sunday");
-                break;
-            default:
-                Console.WriteLine(day + " is not a day");
-                break;
+        //switch (day)
+        //{
+        //    case "Monday":
+        //        Console.WriteLine("Its Monday");
+        //        break;
+        //    case "Tuesday":
+        //        Console.WriteLine("Its Tuesday");
+        //        break;
+        //    case "Wednesday":
+        //        Console.WriteLine("Its Wednesday");
+        //        break;
+        //    case "Thursday":
+        //        Console.WriteLine("Its Thursday");
+        //        break;
+        //    case "Friday":
+        //        Console.WriteLine("Its Friday");
+        //        break;
+        //    case "Saturday":
+        //        Console.WriteLine("Its Saturday");
+        //        break;
+        //    case "Sunday":
+        //        Console.WriteLine("Its Sunday");
+        //        break;
+        //    default:
+        //        Console.WriteLine(day + " is not a day");
+        //        break;
         }
 
         Console.ReadKey();
